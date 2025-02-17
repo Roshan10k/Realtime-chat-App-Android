3d31ca4b62dceee5f40f7aeaa63d299faf2fd1b0
Roshan Khadka <khadkaasd963@gmail.com>
2025-02-17 23:20:42 +0545

    splash activity functionality

---
+++ app/src/main/java/com/example/connect_androidchatapp/ui/activity/SplashActivity.kt
@@ -0,0 +1,32 @@
+package com.example.connect_androidchatapp.ui.activity
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.connect_androidchatapp.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            // Always open LoginActivity
+            val intent = Intent(this@SplashActivity, LoginActivity::class.java)
+            startActivity(intent)
+            finish() // Close the SplashActivity so it doesn't remain in the back stack
+        }, 2200)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
