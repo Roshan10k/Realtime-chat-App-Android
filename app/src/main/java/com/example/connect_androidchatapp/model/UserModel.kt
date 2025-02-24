aa2d3b7fe60b47fdbbc8d4f1720b8e4dd4adc478
Roshan Khadka <khadkaasd963@gmail.com>
2025-02-24 20:37:17 +0545

    creating user model

---
+++ app/src/main/java/com/example/connect_androidchatapp/model/UserModel.kt
@@ -0,0 +1,10 @@
+package com.example.connect_androidchatapp.model
+
+data class UserModel(
+    var userID : String ="",
+    var email: String = "",
+    var name:String="",
+    var password: String = "",
+    var imageUrl: String = "",
+) {
+}
